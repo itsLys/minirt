e3b1c4e33a5e80e840c225970aa046e2ed58572e
Lys <elyesseen@gmail.com>
2025-08-12 12:33:46 +0100

    start printing funcs

--- src/main.c
+++ src/main.c
@@ -12,6 +12,31 @@
 
 #include "minirt.h"
 
+void print_scene_params(t_scene scene)
+{
+	double	light_ratio	= scene.amb_light.ratio;
+	uint8_t r			= scene.amb_light.color.r;
+	uint8_t g			= scene.amb_light.color.g;
+	uint8_t b			= scene.amb_light.color.b;
+	double	cam_x		= scene.cam.pos.x;
+	double	cam_y		= scene.cam.pos.y;
+	double	cam_z		= scene.cam.pos.z;
+	int		light_count	= count_obj(scene.objs, T_LS);
+	int		obj_count	= count_obj(scene.objs, T_CY) + count_objs(scene.objs, T_PL) + count_objs(scene.objs, T_SP);
+	printf("======== MINI RT SCENE ========\n");
+	printf("Ambient Light:	%lf | Color:	(%d, %d, %d)\n", light_ratio, r, g, b);
+	printf("Camera:			(%lf, %lf, %lf)\n", cam_x, cam_y, cam_z);
+	printf("Light Count:	%d\n", light_count);
+	printf("Object Count:	%d\n", obj_count);
+	printf("--------------------------------\n");
+}
+
+void	print_scene(t_data *data)
+{
+	t_scene scene = data->scene;
+	print_scene_params(scene);
+}
+
 int main(int ac, char **av)
 {
 	t_data	data;
@@ -20,5 +45,6 @@ int main(int ac, char **av)
 		return print_error("Wrong args\n"), FAILIURE;
 	if (parse_file(av[1], &data) == ERROR)
 		return (EXIT_FAILURE);
+	print_scene(&data);
 	printf("Hello\n");
 }
