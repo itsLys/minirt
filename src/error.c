481fd20532675cd92aa60d75e10538325245bef4
Lys <elyesseen@gmail.com>
2025-08-12 20:17:24 +0100

    add a condition for printing error messages

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: ihajji <ihajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 15:55:39 by ihajji            #+#    #+#             */
-/*   Updated: 2025/08/12 16:19:49 by ihajji           ###   ########.fr       */
+/*   Updated: 2025/08/12 20:16:58 by ihajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ void print_error(char *err)
 	write(STDERR_FILENO, err, ft_strlen(err));
 }
 
-void	exit_error(char *msg, t_data *data) 
+void	exit_error(char *msg, t_data *data)
 {
 	obj_lst_free(data->scene.obj_list);
-	print_error(msg);
+	if (msg)
+		print_error(msg);
 	exit(FAILIURE);
 }
 
