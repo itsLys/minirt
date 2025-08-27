9ec96745ead90c90a1616c62e6750ad024a56ce4
ysn-elg <yassine.elguaddar99@gmail.com>
2025-08-27 15:10:49 +0100

    camera s movement

--- src/events/keys.c
+++ src/events/keys.c
@@ -6,7 +6,7 @@
 /*   By: yel-guad <yel-guad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 15:58:32 by ihajji            #+#    #+#             */
-/*   Updated: 2025/08/14 16:29:01 by yel-guad         ###   ########.fr       */
+/*   Updated: 2025/08/27 15:09:45 by yel-guad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,20 @@ int	handle_keypress(int code, t_data *data)
 {
 	(void) data;
 	printf("key pressed:	%d\n", code);
+	if (code == 65362) // up
+		data->scene.cam.pos.z -= 0.05;
+	else if (code == 65364) // down
+		data->scene.cam.pos.z += 0.05;
+	else if (code == 65361) // left
+		data->scene.cam.pos.x -= 0.05;
+	else if (code == 65363) // right
+		data->scene.cam.pos.x += 0.05;
+	else if (code == 65365) // PgUp
+		data->scene.cam.pos.y += 0.05;
+	else if (code == 65366) // PgDn
+		data->scene.cam.pos.y -= 0.05;
+	mlx_loop_hook(data->mlx, &render_img, data);
+
 	if (code == XK_Escape)
 		clean_exit(data, SUCCESS);
 	return 0;
