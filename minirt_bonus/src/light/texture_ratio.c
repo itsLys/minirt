e9dfc07797d90ae3cd0281cddef9149272ba5c75
ysn-elg <yassine.elguaddar99@gmail.com>
2025-10-13 15:50:56 +0100

    fix texture ratio for plane

--- minirt_bonus/src/light/texture_ratio.c
+++ minirt_bonus/src/light/texture_ratio.c
@@ -6,7 +6,7 @@
 /*   By: yel-guad <yel-guad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 09:14:21 by yel-guad          #+#    #+#             */
-/*   Updated: 2025/10/10 09:15:15 by yel-guad         ###   ########.fr       */
+/*   Updated: 2025/10/13 15:47:37 by yel-guad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,8 +53,12 @@ static t_vec2	plane_ratio(t_hit hit, t_pl *pl)
 	local = world_to_local(hit, hit.obj->coords);
 	coords.x = local.x / hit.obj->tx->width;
 	coords.y = local.z / hit.obj->tx->height;
-	coords.x = fabs(fmod(coords.x * hit.obj->tiles.x, 1.0));
-	coords.y = fabs(fmod(coords.y * hit.obj->tiles.y, 1.0));
+	coords.x = fmod(coords.x * hit.obj->tiles.x, 1.0);
+	coords.y = fmod(coords.y * hit.obj->tiles.y, 1.0);
+	if (coords.x < 0)
+		coords.x = 1 + coords.x;
+	if (coords.y < 0)
+		coords.y = 1 + coords.y;
 	return (coords);
 }
 
@@ -73,6 +77,21 @@ static t_vec2	cone_ratio(t_hit hit, t_cn *cn)
 	return (coords);
 }
 
+static t_vec2	rectangle_ratio(t_hit hit, t_rc *rc)
+{
+	t_vec3	local;
+	t_vec3	v;
+	t_vec2	coords;
+
+	local = world_to_local(hit, hit.obj->coords);
+	v = vec3_subtract(hit.point, hit.obj->pos);
+	coords.x = vec3_dot(v, hit.obj->coords.forward);
+	coords.y = vec3_dot(v, hit.obj->coords.right);
+	coords.x = (coords.x + rc->width/2.0) / rc->width;
+	coords.y = (coords.y + rc->length/2.0) / rc->length;
+	return (coords);
+}
+
 t_vec2	compute_texture_ratio(t_hit hit)
 {
 	if (hit.obj->type == T_SP)
@@ -83,5 +102,7 @@ t_vec2	compute_texture_ratio(t_hit hit)
 		return (cylinder_ratio(hit, hit.obj->shape));
 	else if (hit.obj->type == T_CN)
 		return (cone_ratio(hit, hit.obj->shape));
+	else if (hit.obj->type == T_RC)
+		return (rectangle_ratio(hit, hit.obj->shape));
 	return ((t_vec2){-1, -1});
 }
