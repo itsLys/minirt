15713423ad8861044de2f38cee356a373a49d2fc
ysn-elg <yassine.elguaddar99@gmail.com>
2025-10-14 16:42:34 +0100

    disable bump map for shadow points (commit n2 to fix a small problem)

--- minirt_bonus/src/hit/trace.c
+++ minirt_bonus/src/hit/trace.c
@@ -6,7 +6,7 @@
 /*   By: yel-guad <yel-guad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 17:01:38 by ihajji            #+#    #+#             */
-/*   Updated: 2025/10/14 16:06:31 by yel-guad         ###   ########.fr       */
+/*   Updated: 2025/10/14 16:39:09 by yel-guad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,12 +49,12 @@ t_rgb	trace_ray(t_ray ray, t_data *data)
 	hit.obj = NULL;
 	obj = *(data->scene.obj_lst);
 	record_hit(&obj, &hit, ray);
+	hit.normal_bumped = hit.normal;
 	if (hit.hit && (hit.obj->tx || hit.obj->bmp))
 	{
 		coords = compute_texture_ratio(hit);
 		if (hit.obj->tx)
 			hit.color = sample_tx_color(coords, hit.obj->tx);
-		hit.normal_bumped = hit.normal;
 		if (hit.obj->bmp)
 			apply_bump_map(&hit, coords);
 	}
