7653fe8e9e92ea59b8605b1cfeebd6c9cdeeb3f0
ysn-elg <yassine.elguaddar99@gmail.com>
2025-10-15 09:16:26 +0100

    set CAM_ROTATE_STEP macros to rotate_spin and _turn

--- minirt_bonus/src/events/handlers/rotate_handler.c
+++ minirt_bonus/src/events/handlers/rotate_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rotate_handler.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ihajji <ihajji@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: yel-guad <yel-guad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/31 10:38:04 by ihajji            #+#    #+#             */
-/*   Updated: 2025/09/24 13:14:52 by ihajji           ###   ########.fr       */
+/*   Updated: 2025/10/15 09:14:51 by yel-guad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@ void	rotate_tilt(int code, t_local_coords *coords)
 {
 	if (code == XK_s)
 		coords->forward = rotate(coords->forward,
-				coords->right, ROTATE_STEP);
+				coords->right, CAM_ROTATE_STEP);
 	else if (code == XK_w)
 		coords->forward = rotate(coords->forward,
-				coords->right, ROTATE_STEP * -1);
+				coords->right, CAM_ROTATE_STEP * -1);
 	coords->up = vec3_cross(coords->right, coords->forward);
 }
 
 void	rotate_turn(int code, t_local_coords *coords)
 {
 	if (code == XK_e)
-		coords->up = rotate(coords->up, coords->forward, ROTATE_STEP);
+		coords->up = rotate(coords->up, coords->forward, CAM_ROTATE_STEP);
 	else if (code == XK_q)
-		coords->up = rotate(coords->up, coords->forward, ROTATE_STEP * -1);
+		coords->up = rotate(coords->up, coords->forward, CAM_ROTATE_STEP * -1);
 	coords->right = vec3_norm(vec3_cross(coords->forward, coords->up));
 }
 
