df2effe26b1f1a1478e2eef2594de87f110f97ab
ysn-elg <yassine.elguaddar99@gmail.com>
2025-10-17 18:15:19 +0100

    fix rc texture (++)

--- minirt_bonus/src/light/texture_ratio.c
+++ minirt_bonus/src/light/texture_ratio.c
@@ -6,7 +6,7 @@
 /*   By: yel-guad <yel-guad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 09:14:21 by yel-guad          #+#    #+#             */
-/*   Updated: 2025/10/17 17:57:58 by yel-guad         ###   ########.fr       */
+/*   Updated: 2025/10/17 18:14:08 by yel-guad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,5 +87,9 @@ t_vec2	rectangle_ratio(t_hit hit, t_rc *rc)
 	coords.x = (local.x + rc->width / 2.0) / rc->width;
 	coords.y = fmod(coords.y * hit.obj->tiles.y, 1.0);
 	coords.x = fmod(coords.x * hit.obj->tiles.x, 1.0);
+	if (coords.x < 0)
+		coords.x = 1 + coords.x;
+	if (coords.y < 0)
+		coords.y = 1 + coords.y;
 	return (coords);
 }
